35e14f9390677b0c5c29fb058ac3063ef720932b
JonNavaridas <jon.navaridas@alumni.mondragon.edu>
2021-12-20 11:34:26 +0100

    Fix button and images
    Change image routes and a button's id was duplicated.

--- G&C/src/main/java/org/eco/mubisoft/good_and_cheap/application/control/LoginController.java
+++ G&C/src/main/java/org/eco/mubisoft/good_and_cheap/application/control/LoginController.java
@@ -1,29 +1,15 @@
 package org.eco.mubisoft.good_and_cheap.application.control;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collection;
-
-import static java.util.Arrays.stream;
 
 @Slf4j
 @Controller
@@ -32,9 +18,18 @@ import static java.util.Arrays.stream;
 public class LoginController {
 
     @GetMapping("/sign-in")
-    public String getLoginForm() {
+    public void getLoginForm(HttpServletResponse response) throws IOException {
         log.info("Request login form");
-        return "/login/login_form";
+        response.sendRedirect("/login/login_form");
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        log.info("Login user out");
+        HttpSession session = request.getSession();
+        session.removeAttribute("accessToken");
+        session.removeAttribute("refreshToken");
+        response.sendRedirect("/");
     }
 
 }
