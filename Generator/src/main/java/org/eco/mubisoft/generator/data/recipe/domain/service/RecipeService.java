6efc75c9b9267cd5a41b2417aa8a8c51926e2794
JonNavaridas <jon.navaridas@alumni.mondragon.edu>
2022-01-18 10:39:49 +0100

    Ingredient Generation

--- Generator/src/main/java/org/eco/mubisoft/generator/data/recipe/domain/service/RecipeService.java
+++ Generator/src/main/java/org/eco/mubisoft/generator/data/recipe/domain/service/RecipeService.java
@@ -2,8 +2,9 @@ package org.eco.mubisoft.generator.data.recipe.domain.service;
 
 public interface RecipeService {
 
-        void generateRecipeFlags();
-        void generateRecipes(int quantity);
-        void deleteAll();
+    void generateRecipeFlags();
+    void generateRecipes(int quantity);
+    void generateIngredients();
+    void deleteAll();
 
 }
