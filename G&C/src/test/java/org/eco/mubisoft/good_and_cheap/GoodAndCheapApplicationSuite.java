db0b654a7740911a9961d0bb12b428b47962cfb6
JonNavaridas <jon.navaridas@alumni.mondragon.edu>
2022-01-26 08:54:57 +0100

    Add all packages to test suite

--- G&C/src/test/java/org/eco/mubisoft/good_and_cheap/GoodAndCheapApplicationSuite.java
+++ G&C/src/test/java/org/eco/mubisoft/good_and_cheap/GoodAndCheapApplicationSuite.java
@@ -6,7 +6,10 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SelectPackages({
-        "org.eco.mubisoft.good_and_cheap.application"
+        "org.eco.mubisoft.good_and_cheap.application",
+        "org.eco.mubisoft.good_and_cheap.product",
+        "org.eco.mubisoft.good_and_cheap.recipe",
+        "org.eco.mubisoft.good_and_cheap.control"
 })
 @SuiteDisplayName("Good and Cheap application's tests")
 public class GoodAndCheapApplicationSuite {
